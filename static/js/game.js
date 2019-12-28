c2054e94378bd75f0baef581dfe8649d7e175cfd
Harnet69 <harnet69@gmail.com>
2019-12-28 20:14:44 +0100

    Added is cell ocupied validation

--- static/js/game.js
+++ static/js/game.js
@@ -1,28 +1,51 @@
 // alert('You should write code into "static/js/game.js" to make it work');
-const player1 = 'X';
-const player2 = 'O';
-let pl = true;
+const player1 = 'O';
+const player2 = 'X';
+let pl = 2;
 
 function player_turn(){
 let my_cells = document.getElementsByClassName('game-cell');
     for(let cell of my_cells){
         cell.addEventListener('click', function () {
-            my_click(cell);
+            if (isCellOccupied(cell)) {
+                let player = myClick(cell);
+                let cell_coord = getCellCoord(cell);
+                console.log(player, cell_coord);
+            }
+            else{
+                alert('This cell is occupied. Try another!')
+            }
         } );
     }
 }
 
 
-function my_click (cell) {
-    if (pl) {
+function myClick (cell) {
+    if (pl === 1) {
         cell.textContent = player1;
         cell.classList.add('selected');
-        pl = false;
-    } else {
+        pl = 2;
+    }
+    else{
         cell.textContent = player2;
         cell.classList.add('selected');
-        pl = true;
+        pl = 1;
     }
+    return pl;
+}
+
+function getCellCoord(cell){
+    let dataCoordinateX = cell.getAttribute('data-coordinate-x');
+    let dataCoordinateY = cell.getAttribute('data-coordinate-y');
+    return [+dataCoordinateX, +dataCoordinateY];
+}
+
+// check is the cell occupied
+function isCellOccupied(cell){
+    if(cell.classList.contains('selected')){
+        return false;
+    }
+    return true;
 }
 
 player_turn();
