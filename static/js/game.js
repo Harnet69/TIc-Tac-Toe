97547ce96c37ef3b6e65da7aa422c4d7bef11d50
Harnet69 <harnet69@gmail.com>
2019-12-28 17:36:47 +0100

    Added iteration of players functionality

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,28 @@
-alert('You should write code into "static/js/game.js" to make it work');
+// alert('You should write code into "static/js/game.js" to make it work');
+const player1 = 'X';
+const player2 = 'O';
+let pl = true;
+
+function player_turn(){
+let my_cells = document.getElementsByClassName('game-cell');
+    for(let cell of my_cells){
+        cell.addEventListener('click', function () {
+            my_click(cell);
+        } );
+    }
+}
+
+
+function my_click (cell) {
+    if (pl) {
+        cell.textContent = player1;
+        cell.classList.add('selected');
+        pl = false;
+    } else {
+        cell.textContent = player2;
+        cell.classList.add('selected');
+        pl = true;
+    }
+}
+
+player_turn();
